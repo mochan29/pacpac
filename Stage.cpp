2fbb5751f57f473f1b7026bf71bea68c1e81086f
Moe Ikeda <230090@jc-21.jp>
2024-01-25 15:21:20 +0900

    読み込もうとはした

--- Stage.cpp
+++ Stage.cpp
@@ -7,6 +7,36 @@ void Stage::Initialize()
 	assert(hFloor_ >= 0);
 	hWall_ = Model::Load("Model\\Wall.fbx");
 	assert(hWall_ >= 0);
+
+	for (int x = 0; x < FLOORX; x++)
+	{
+		for (int z = 0; z < FLOORZ; z++)
+		{
+			stage[x][z] = 0;//?
+		}
+	}
+
+	inputFile_.open(fname_, std::ios::in);
+	if (inputFile_.fail())
+	{
+		exit(0);
+	}
+
+	string oneline;
+	while (getline(inputFile_, oneline))
+	{
+		inputList.push_back(oneline);
+	}
+
+	for (int x = 0; x < FLOORX; x++)
+	{
+		for (int z = 0; z < FLOORZ; z++)
+		{
+			if(inputList[x][z]!=',')
+			stage[x][z]=inputList[x][z] - '0';//stage‚ð‰Šú‰»‚µ‚Ä‚©‚ç‚¢‚ê‚È‚¢‚Æ‚¾‚ß‚ç‚µ‚¢
+		}
+	}
+
 }
 
 void Stage::Update()
@@ -17,19 +47,42 @@ void Stage::Draw()
 {
 	Transform floorTrans;
 	Transform wallTrans;
-	for (int z = 0; z < FLOORZ; z++)
+	/*for (int z = 0; z < FLOORZ; z++)
 	{
 		for (int x = 0; x <FLOORX; x++)
+		{
+			floorTrans.position_ = { float(x - 5),0,float(z - 5)};
+			wallTrans.position_ = { float(x - 5),0,float(z - 5)};
+			Model::SetTransform(hFloor_, floorTrans);
+			Model::SetTransform(hWall_, wallTrans);
+			if (z == 0 || z == FLOORZ-1||x == 0||x ==FLOORX-1)
+			{
+				Model::Draw(hWall_);
+			}
+			else
+			{
+				Model::Draw(hFloor_);
+			}
+		}
+	}*/
+
+	for (int x = 0; x < FLOORX; x++)
+	{
+		for (int z = 0; z < FLOORZ; z++)
 		{
 			floorTrans.position_ = { float(x - 5),0,float(z - 5) };
 			wallTrans.position_ = { float(x - 5),0,float(z - 5) };
 			Model::SetTransform(hFloor_, floorTrans);
 			Model::SetTransform(hWall_, wallTrans);
-			Model::Draw(hFloor_);
-			if (z == 0 || z == FLOORZ-1||x == 0||x ==FLOORX-1)
+			if (stage[x][z]==0)
 			{
 				Model::Draw(hWall_);
 			}
+			else
+			{
+				Model::Draw(hFloor_);
+			}
 		}
 	}
+
 }
--- Stage.h
+++ Stage.h
@@ -1,17 +1,34 @@
 #pragma once
 #include "Engine/GameObject.h"
+#include <filesystem>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+using std::getline;
+using std::stringstream;
+
 
 namespace
 {
-	const int FLOORZ = 15;
 	const int FLOORX = 15;
+	const int FLOORZ = 15;
 }
 
-class Stage :
+
+class Stage:
     public GameObject
 {
 	int hFloor_;
 	int hWall_;
+	ifstream inputFile_;
+	string fname_ = "stage.csv";
+	vector<string>inputList;
+	vector<vector<int>> stage;
 public:
 	Stage(GameObject* parent) :GameObject(parent, "Stage"),hFloor_(-1),hWall_(-1) {};
 	void Initialize() override;
